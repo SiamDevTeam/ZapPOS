8064ba305c9e5386a4cdd05d0cdf1046382e5830
Monthira Chayabanjonglerd <monthira.cha@ascendcorp.com>
2025-06-09 21:38:57 +0700

    add iOS files

---
+++ iosApp/ZapPOS/ZapPOSTests/ZapPOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  ZapPOSTests.swift
+//  ZapPOSTests
+//
+//  Created by Monthira.Cha on 6/6/2568 BE.
+//
+
+import Testing
+
+struct ZapPOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
