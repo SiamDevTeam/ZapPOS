8f10b3c037f258ad3b960c3a1d5a8217f93e58e7
Monthira Chayabanjonglerd <acaideddesign3@gmail.com>
2025-07-05 13:49:28 +0700

    refactor: update LoginScreen layout and add new app logo

--- composeApp/src/commonMain/kotlin/org/siamdev/zappos/screen/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/org/siamdev/zappos/screen/LoginScreen.kt
@@ -1,21 +1,25 @@
 package org.siamdev.zappos.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.siamdev.zappos.view.MaterialButton
 import zappos.composeapp.generated.resources.Res
 import zappos.composeapp.generated.resources.compose_multiplatform
+import zappos.composeapp.generated.resources.ic_add
+import zappos.composeapp.generated.resources.ic_app_logo
 
 @Composable
 fun LoginScreen(onButtonClick: () -> Unit) {
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().background(color = Color.White),
         contentAlignment = Alignment.Center
     ) {
         Column(
@@ -24,11 +28,10 @@ fun LoginScreen(onButtonClick: () -> Unit) {
             modifier = Modifier.fillMaxWidth()
         ) {
             Image(
-                painter = painterResource(Res.drawable.compose_multiplatform),
+                painter = painterResource(Res.drawable.ic_app_logo),
                 contentDescription = "Logo",
-                modifier = Modifier.size(120.dp)
+                modifier = Modifier.size(180.dp)
             )
-            Spacer(modifier = Modifier.height(32.dp))
             MaterialButton(
                 text = "Login with Nostr account",
                 onClick = onButtonClick
