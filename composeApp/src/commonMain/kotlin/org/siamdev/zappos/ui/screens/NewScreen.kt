f6dab7453d7498dcf13b79422048cee67c7a7dd3
rushmi0 <gcalku@gmail.com>
2025-11-29 21:34:37 +0700

    feat: add NewScreen composable with Preview

--- composeApp/src/commonMain/kotlin/org/siamdev/zappos/ui/screens/NewScreen.kt
+++ composeApp/src/commonMain/kotlin/org/siamdev/zappos/ui/screens/NewScreen.kt
@@ -1,10 +1,16 @@
-/*
 package org.siamdev.zappos.ui.screens
 
 import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
-class NewScreen {
+fun NewScreen() {
+
+}
 
 
-}*/
+@Preview
+@Composable
+fun NewScreenPreview() {
+    NewScreen()
+}
