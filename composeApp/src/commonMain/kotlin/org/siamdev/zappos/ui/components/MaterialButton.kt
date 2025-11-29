0d88867233fcecb33d3e89152c24e7fe6843fe85
rushmi0 <gcalku@gmail.com>
2025-11-29 22:51:06 +0700

    demo: add start and end icon support to MaterialButton

--- composeApp/src/commonMain/kotlin/org/siamdev/zappos/ui/components/MaterialButton.kt
+++ composeApp/src/commonMain/kotlin/org/siamdev/zappos/ui/components/MaterialButton.kt
@@ -27,9 +27,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Article
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -96,7 +100,10 @@ fun MaterialButton(
 @Composable
 fun MaterialButtonPreview() {
     MaterialButton(
+        modifier = Modifier.width(250.dp),
         text = "Preview Button",
+        iconStart = Icons.AutoMirrored.Filled.Article,
+        iconEnd = Icons.AutoMirrored.Filled.KeyboardArrowRight,
         onClick = {}
     )
 }
